3cdbaeff11082840b1002d6d0fd98181ace38edb
Назаренко Алексей <58364366+Alexei-russia@users.noreply.github.com>
2022-09-17 09:43:32 +0300

    загрузка скриптов

---
+++ Scripts/UC02_SearchFlight/Action.c
@@ -0,0 +1,142 @@
+Action()
+{
+
+/*Correlation comment - Do not change!  Original value='134644.527339004zQcfHHHpiHftVfftpzzHcf' Name ='userSession' Type ='ResponseBased'*/
+	web_reg_save_param_attrib(
+		"ParamName=userSession",
+		"TagName=input",
+		"Extract=value",
+		"Name=userSession",
+		"Type=hidden",
+		SEARCH_FILTERS,
+		"IgnoreRedirections=No",
+		"RequestUrl=*/nav.pl*",
+		LAST);
+		
+	lr_start_transaction("home Page");
+		
+	web_reg_find("Fail=NotFound",
+		"Text=sign up now",
+		LAST);
+
+	web_url("WebTours", 
+		"URL=http://localhost:1080/WebTours", 
+		"TargetFrame=", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=", 
+		"Snapshot=t1.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	lr_end_transaction("home Page", LR_AUTO);
+
+	web_set_sockets_option("SSL_VERSION", "AUTO");
+
+	lr_start_transaction("click login");
+	
+	web_reg_find("Fail=NotFound",
+		"Text/IC=User password was correct",
+		LAST);
+
+	web_submit_data("login.pl",
+		"Action=http://localhost:1080/cgi-bin/login.pl",
+		"Method=POST",
+		"TargetFrame=body",
+		"RecContentType=text/html",
+		"Referer=http://localhost:1080/cgi-bin/nav.pl?in=home",
+		"Snapshot=t2.inf",
+		"Mode=HTML",
+		ITEMDATA,
+		"Name=userSession", "Value={userSession}", ENDITEM,
+		"Name=username", "Value={username}", ENDITEM,
+		"Name=password", "Value={password}", ENDITEM,
+		"Name=JSFormSubmit", "Value=off", ENDITEM,
+		"Name=login.x", "Value=52", ENDITEM,
+		"Name=login.y", "Value=6", ENDITEM,
+		LAST);
+
+	lr_end_transaction("click login",LR_AUTO);
+
+	lr_think_time(20);
+
+	lr_start_transaction("click flights");
+	
+	web_reg_find("Fail=NotFound",
+		"Text=Departure City",
+		LAST);
+
+	web_url("Search Flights Button", 
+		"URL=http://localhost:1080/cgi-bin/welcome.pl?page=search", 
+		"TargetFrame=body", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/nav.pl?page=menu&in=home", 
+		"Snapshot=t3.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	lr_end_transaction("click flights",LR_AUTO);
+
+	lr_think_time(48);
+	
+	web_reg_save_param("outboundFlight",
+	"LB=outboundFlight\" value=\"",
+	"RB=\"",
+	LAST);
+
+	lr_start_transaction("find flight");
+	
+	web_reg_find("Fail=NotFound",
+		"Text/IC=Flight departing from",
+		LAST);
+
+
+	web_submit_data("reservations.pl", 
+		"Action=http://localhost:1080/cgi-bin/reservations.pl", 
+		"Method=POST", 
+		"TargetFrame=", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/reservations.pl?page=welcome", 
+		"Snapshot=t4.inf", 
+		"Mode=HTML", 
+		ITEMDATA, 
+		"Name=advanceDiscount", "Value=0", ENDITEM, 
+		"Name=depart", "Value={depart}", ENDITEM, 
+		"Name=departDate", "Value={departDate}", ENDITEM, 
+		"Name=arrive", "Value={arrive}", ENDITEM, 
+		"Name=returnDate", "Value={returnDate}", ENDITEM, 
+		"Name=numPassengers", "Value=1", ENDITEM, 
+		"Name=seatPref", "Value=None", ENDITEM, 
+		"Name=seatType", "Value=Coach", ENDITEM, 
+		"Name=.cgifields", "Value=roundtrip", ENDITEM, 
+		"Name=.cgifields", "Value=seatType", ENDITEM, 
+		"Name=.cgifields", "Value=seatPref", ENDITEM, 
+		"Name=findFlights.x", "Value=41", ENDITEM, 
+		"Name=findFlights.y", "Value=8", ENDITEM, 
+		LAST);
+
+	lr_end_transaction("find flight",LR_AUTO);
+
+	lr_think_time(21);
+
+	lr_start_transaction("sign off");
+	
+	web_reg_find("Fail=NotFound",
+		"Text=Welcome to the Web Tours site.",
+		LAST);
+
+	web_url("SignOff Button", 
+		"URL=http://localhost:1080/cgi-bin/welcome.pl?signOff=1", 
+		"TargetFrame=body", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/nav.pl?page=menu&in=home", 
+		"Snapshot=t10.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	lr_end_transaction("sign off",LR_AUTO);
+
+	return 0;
+}
---
+++ Scripts/UC04_CheckItinarery/Action.c
@@ -0,0 +1,114 @@
+Action()
+{
+
+/*Correlation comment - Do not change!  Original value='134644.527339004zQcfHHHpiHftVfftpzzHcf' Name ='userSession' Type ='ResponseBased'*/
+	web_reg_save_param_attrib(
+		"ParamName=userSession",
+		"TagName=input",
+		"Extract=value",
+		"Name=userSession",
+		"Type=hidden",
+		SEARCH_FILTERS,
+		"IgnoreRedirections=No",
+		"RequestUrl=*/nav.pl*",
+		LAST);
+		
+	lr_start_transaction("home Page");
+		
+	web_reg_find("Fail=NotFound",
+		"Text=sign up now",
+		LAST);
+
+	web_url("WebTours", 
+		"URL=http://localhost:1080/WebTours", 
+		"TargetFrame=", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=", 
+		"Snapshot=t1.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	lr_end_transaction("home Page", LR_AUTO);
+
+	web_set_sockets_option("SSL_VERSION", "AUTO");
+
+	lr_start_transaction("click login");
+	
+	web_reg_find("Fail=NotFound",
+		"Text/IC=User password was correct",
+		LAST);
+
+	web_submit_data("login.pl",
+		"Action=http://localhost:1080/cgi-bin/login.pl",
+		"Method=POST",
+		"TargetFrame=body",
+		"RecContentType=text/html",
+		"Referer=http://localhost:1080/cgi-bin/nav.pl?in=home",
+		"Snapshot=t2.inf",
+		"Mode=HTML",
+		ITEMDATA,
+		"Name=userSession", "Value={userSession}", ENDITEM,
+		"Name=username", "Value={username}", ENDITEM,
+		"Name=password", "Value={password}", ENDITEM,
+		"Name=JSFormSubmit", "Value=off", ENDITEM,
+		"Name=login.x", "Value=52", ENDITEM,
+		"Name=login.y", "Value=6", ENDITEM,
+		LAST);
+
+	lr_end_transaction("click login",LR_AUTO);
+
+	lr_think_time(20);
+
+	lr_start_transaction("click itinerary");
+	
+	web_reg_find("Fail=NotFound",
+		"Text=scheduled flights.",
+		LAST);
+
+/*Correlation comment - Do not change!  Original value='210297424-100296-JB' Name ='flightID' Type ='ResponseBased'*/
+	web_reg_save_param_attrib(
+		"ParamName=flightID",
+		"TagName=input",
+		"Extract=value",
+		"Name=flightID",
+		"Type=hidden",
+		SEARCH_FILTERS,
+		"IgnoreRedirections=No",
+		"RequestUrl=*/itinerary.pl*",
+		LAST);
+
+	web_url("Itinerary Button", 
+		"URL=http://localhost:1080/cgi-bin/welcome.pl?page=itinerary", 
+		"TargetFrame=body", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/nav.pl?page=menu&in=flights", 
+		"Snapshot=t7.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	lr_end_transaction("click itinerary",LR_AUTO);
+
+	lr_think_time(21);
+
+	lr_start_transaction("sign off");
+	
+	web_reg_find("Fail=NotFound",
+		"Text=Welcome to the Web Tours site.",
+		LAST);
+
+	web_url("SignOff Button", 
+		"URL=http://localhost:1080/cgi-bin/welcome.pl?signOff=1", 
+		"TargetFrame=body", 
+		"Resource=0", 
+		"RecContentType=text/html", 
+		"Referer=http://localhost:1080/cgi-bin/nav.pl?page=menu&in=home", 
+		"Snapshot=t10.inf", 
+		"Mode=HTML", 
+		LAST);
+
+	lr_end_transaction("sign off",LR_AUTO);
+
+	return 0;
+}
